6e852e53afaf59e4c2063e591f2099f55547beb3
JBDCE <61785226+JBDCE@users.noreply.github.com>
2023-12-19 23:30:04 +0100

    Finish up line parser and implement is_valid func

--- src/Day2/Game.cpp
+++ src/Day2/Game.cpp
@@ -17,6 +17,7 @@ private:
 
 public:
     Game(string input_line);
+    bool Game::is_valid(int maxRed, int maxGreen, int maxBlue);
 };
 
 Game::Game(string input_line)
@@ -34,4 +35,49 @@ Game::Game(string input_line)
     // Seperate the games into a list
     std::cout << line << std::endl;
 
+    regex drawPattern("\\d.+?(?:;|$)");
+    std::sregex_iterator iterator(line.begin(), line.end(), drawPattern);
+    std::sregex_iterator end;
+
+    // Loop over matches
+    while (iterator != end) {
+        std::smatch match = *iterator;
+        string match_string = match.str();
+        regex bluePattern("\\d+ blue");
+        regex redPattern("\\d+ red");
+        regex greenPattern("\\d+ green");
+
+        int blue = 0;
+        int red = 0;
+        int green = 0;
+
+        std::smatch colorMatch;
+        if (std::regex_search(match_string, colorMatch, bluePattern)){
+            string n_string = colorMatch.str();
+            blue = std::stoi(n_string.substr(0, n_string.length()-5));
+        }
+        if (std::regex_search(match_string, colorMatch, redPattern)){
+            string n_string = colorMatch.str();
+            red = std::stoi(n_string.substr(0, n_string.length()-4));
+        }
+        if (std::regex_search(match_string, colorMatch, greenPattern)){
+            string n_string = colorMatch.str();
+            green = std::stoi(n_string.substr(0, n_string.length()-6));
+        }
+
+        std::cout << "Match: " << match.str() << std::endl << "Blue: " << blue << " Red: " << red << " Green: " << green << std::endl;
+        draws.push_back(Draw(red, green, blue));
+
+        ++iterator;
+    }
+
+}
+
+bool Game::is_valid(int maxRed, int maxGreen, int maxBlue) {
+    for (Draw draw : this->draws) {
+        if (draw.get_red() > maxRed) return false;
+        if (draw.get_green() > maxGreen) return false;
+        if (draw.get_blue() > maxBlue) return false;
+    }
+    return true;
 }
